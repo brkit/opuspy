5d91a74ca114f4eccf95644d0b916c0f475b2281
dzgreen <dominik2711@gmail.com>
2026-05-23 12:51:06 +0200

    bump version to 0.10.0 and add set_sap_scripting_to_allowed to exports

--- src/opuspy/__init__.py
+++ src/opuspy/__init__.py
@@ -1,5 +1,6 @@
 from .opus import (
     is_sap_scripting_allowed,
+    set_sap_scripting_to_allowed,
     sap_connection,
     say_hello_from_opuspy,
     start_opus,
@@ -10,4 +11,5 @@ __all__ = [
     "say_hello_from_opuspy",
     "sap_connection",
     "is_sap_scripting_allowed",
+    "set_sap_scripting_to_allowed",
 ]
